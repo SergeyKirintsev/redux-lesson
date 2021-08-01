11f10c8d3a2991df71bc0b8421c7ce031a33e8a8
SergeyKirintsev <kserg80@gmail.com>
2021-08-01 16:19:39 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { addCustomerAction, removeCustomerAction } from './store/customerReducer'
+import { fetchCustomers } from './asyncActions/customers'
 
 function App() {
+  const dispatch = useDispatch()
+  const cash = useSelector(state => state.cash.cash)
+  const customers = useSelector(state => state.customers.customers)
+
+
+  const addCash = () => {
+    dispatch({
+      type: 'ADD_CASH',
+      payload: 1
+    })
+  }
+
+  const getCash = () => {
+    dispatch({
+      type: 'GET_CASH',
+      payload: 1
+    })
+  }
+
+  const addCustomer = name => {
+    const customer = {
+      name,
+      id: Date.now()
+    }
+    dispatch(addCustomerAction(customer))
+  }
+
+  const removeCustomer = (id) => {
+    dispatch(removeCustomerAction(id))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>Денежка: {cash}</p>
+      <button onClick={() => addCash()} style={{ marginRight: '10px' }}>Добавить</button>
+      <button onClick={() => getCash()}>Снять</button>
+      <div style={{ marginTop: '20px' }}>
+        <button onClick={() => addCustomer(prompt())} style={{ marginRight: '10px' }}>Добавить клиента</button>
+        <button onClick={() => getCash()}>Удалить клиента</button>
+        <button onClick={() => dispatch(fetchCustomers())}>Получить клиентов из базы</button>
+        {/* <button onClick={() => fetchCustomers()(dispatch)}>Получить клиентов из базы</button> */}
+        {customers.length > 0 ?
+          <div>
+            {customers.map(customer =>
+              <p onClick={() => removeCustomer(customer.id)} style={{ padding: '5px' }}>{customer.name}</p>
+            )}
+          </div>
+          :
+          <p>Клиенты отсутствуют</p>
+        }
+      </div>
     </div>
   );
 }
---
+++ src/store/customerReducer.js
@@ -0,0 +1,35 @@
+const defaultState = {
+  customers: []
+}
+
+const ADD_CUSTOMER = 'ADD_CUSTOMER'
+const ADD_MANY_CUSTOMERS = 'ADD_MANY_CUSTOMERS'
+const REMOVE_CUSTOMERS = 'REMOVE_CUSTOMERS'
+
+export const customerReducer = (state = defaultState, action) => {
+  switch (action.type) {
+    case ADD_MANY_CUSTOMERS:
+      return { ...state, customers: [...state.customers, ...action.payload] }
+    case ADD_CUSTOMER:
+      return { ...state, customers: [...state.customers, action.payload] }
+    case REMOVE_CUSTOMERS:
+      return { ...state, customers: state.customers.filter(customer => customer.id !== action.payload) }
+    default:
+      return state
+  }
+}
+
+export const addCustomerAction = (payload) => ({
+  type: ADD_CUSTOMER,
+  payload
+})
+
+export const addManyCustomerAction = (payload) => ({
+  type: ADD_MANY_CUSTOMERS,
+  payload
+})
+
+export const removeCustomerAction = (payload) => ({
+  type: REMOVE_CUSTOMERS,
+  payload
+})
